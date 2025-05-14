b5a902fbe6e20cc467e95a799d004a28dd53484d
adithyabandara594 <adithyabandara112@gmail.com>
2025-05-14 07:15:33 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "KngFXD6I#tZMx253RfeYdFrYyNdX3WgZpFQSMvr-zzgh6Tdx8GrQ",
+SESSION_ID: process.env.SESSION_ID || "𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=rjh2EIJA#P-xfE7_KCnNVVIL16j2IxMKbApJuWEi48DZUMII8pHY",
 ALIVE_IMG : process.env.ALIVE_IMG || "https://telegra.ph/file/38a4ba6f7a51c15f5feaf.jpg",
 ALIVE_MSG : process.env.ALIVE_MSG || "Hello i am ASITGA-MD i am alive now ❤️‍🩹@&",    
 };
